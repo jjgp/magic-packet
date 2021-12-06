098c415ea5949498f271dbe960802b249d7db16c
Jason Prasad <jasongprasad@gmail.com>
2021-12-06 16:08:04 +0000

    Fix read/write of pem

--- mlops/terraform/spot_instance/main.tf
+++ mlops/terraform/spot_instance/main.tf
@@ -15,6 +15,7 @@ provider "aws" {
   region = var.region
 }
 
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/key_pair
 resource "aws_key_pair" "key_pair" {
   key_name   = var.key_name
   public_key = var.public_key
@@ -24,7 +25,9 @@ resource "aws_key_pair" "key_pair" {
   }
 }
 
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/spot_instance_request
 resource "aws_spot_instance_request" "instance_request" {
+  # Currently the AMI and instance may only work in eu-west-1
   ami           = "ami-015b1508a2ff2c65a"
   instance_type = "t2.micro"
   key_name      = var.key_name
