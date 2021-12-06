3aaf1e3b3745ace629e86ad69c152869a089e7f8
Jason Prasad <jasongprasad@gmail.com>
2021-12-06 23:31:34 +0000

    Add setup to be able to connect to EC2

--- mlops/terraform/spot_instance/main.tf
+++ mlops/terraform/spot_instance/main.tf
@@ -7,16 +7,93 @@ terraform {
   }
 }
 
-locals {
-  tag_name = "magic_packet_spot_instance"
-}
-
 provider "aws" {
   region = var.region
 }
 
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/key_pair
-resource "aws_key_pair" "key_pair" {
+data "aws_availability_zones" "available" {}
+
+locals {
+  availability_zone = data.aws_availability_zones.available.names[0]
+  # Acronym for magic packet spot instance
+  tag_name = "mpsi"
+}
+
+resource "aws_vpc" "mpsi_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = local.tag_name
+  }
+}
+
+resource "aws_internet_gateway" "mpsi_internet_gateway" {
+  vpc_id = aws_vpc.mpsi_vpc.id
+
+  tags = {
+    Name = local.tag_name
+  }
+}
+
+resource "aws_route_table" "mpsi_route_table" {
+  vpc_id = aws_vpc.mpsi_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.mpsi_internet_gateway.id
+  }
+
+  tags = {
+    Name = local.tag_name
+  }
+}
+
+resource "aws_subnet" "mpsi_subnet" {
+  availability_zone = local.availability_zone
+  cidr_block        = "10.0.1.0/24"
+  vpc_id            = aws_vpc.mpsi_vpc.id
+
+  tags = {
+    Name = local.tag_name
+  }
+}
+
+resource "aws_route_table_association" "mpsi_route_table_association" {
+  subnet_id      = aws_subnet.mpsi_subnet.id
+  route_table_id = aws_route_table.mpsi_route_table.id
+}
+
+resource "aws_security_group" "mpsi_security_group" {
+  vpc_id = aws_vpc.mpsi_vpc.id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 8888
+    to_port     = 8888
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = local.tag_name
+  }
+}
+
+resource "aws_key_pair" "mpsi_key_pair" {
   key_name   = var.key_name
   public_key = var.public_key
 
@@ -25,12 +102,14 @@ resource "aws_key_pair" "key_pair" {
   }
 }
 
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/spot_instance_request
-resource "aws_spot_instance_request" "instance_request" {
-  # Currently the AMI and instance may only work in eu-west-1
-  ami           = "ami-015b1508a2ff2c65a"
-  instance_type = "t2.micro"
-  key_name      = var.key_name
+resource "aws_spot_instance_request" "mpsi_spot_instance_request" {
+  ami                         = var.ami
+  associate_public_ip_address = true
+  availability_zone           = local.availability_zone
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  subnet_id                   = aws_subnet.mpsi_subnet.id
+  vpc_security_group_ids      = [aws_security_group.mpsi_security_group.id]
 
   tags = {
     Name = local.tag_name
--- mlops/terraform/spot_instance/variables.tf
+++ mlops/terraform/spot_instance/variables.tf
@@ -1,3 +1,15 @@
+variable "ami" {
+  default     = "ami-015b1508a2ff2c65a"
+  description = "The AMI to use for the instance"
+  type        = string
+}
+
+variable "instance_type" {
+  default     = "t2.micro"
+  description = "The EC2 instance type to use"
+  type        = string
+}
+
 variable "key_name" {
   description = "The name to associate with the public key"
   type        = string
