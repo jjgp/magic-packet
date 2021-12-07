a5e245f544720222de5e90df73170a1f52eeebef
Jason Prasad <jasongprasad@gmail.com>
2021-12-07 10:03:03 +0000

    Refactor common functions to mlops module

---
+++ mlops/terraform_cli.py
@@ -0,0 +1,12 @@
+def variables_dict_to_options(variables):
+    values = [f'"{key}={value}"' for key, value in variables.items()]
+    flags = ["-var"] * len(values)
+    return " ".join([arg for tup in zip(flags, values) for arg in tup])
+
+
+def variables_dict_to_tvars(variables, path="terraform.tfvars"):
+    with open(path, "w") as f:
+        for key, value in variables.items():
+            # TODO: will need to extend this logic for other types
+            if isinstance(value, str):
+                f.write(f'{key}="{value}"\n')
