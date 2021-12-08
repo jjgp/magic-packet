35a14bef45e7954f4e9b82eadc918cd9ffcb9d02
Jason Prasad <jasongprasad@gmail.com>
2021-12-08 11:35:41 +0000

    Change wording around pem

--- mlops/key_pair.py
+++ mlops/key_pair.py
@@ -4,10 +4,10 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 
 
-def read_pem_public_key(pem_private_key_path, password=None):
-    key_path = os.path.expanduser(pem_private_key_path)
-    with open(key_path, "rb") as key_file:
-        private_key = serialization.load_pem_private_key(key_file.read(), password)
+def read_public_key_from_pem(pem_private_key_path, password=None):
+    pem_path = os.path.expanduser(pem_private_key_path)
+    with open(pem_path, "rb") as pem_file:
+        private_key = serialization.load_pem_private_key(pem_file.read(), password)
         public_key = private_key.public_key()
         public_bytes = public_key.public_bytes(
             serialization.Encoding.OpenSSH, serialization.PublicFormat.OpenSSH
@@ -15,9 +15,9 @@ def read_pem_public_key(pem_private_key_path, password=None):
         return public_bytes.decode("utf-8")
 
 
-def write_pem_private_key(pem_private_key_path, password=None):
-    key_path = os.path.expanduser(pem_private_key_path)
-    with open(key_path, "wb") as key_file:
+def write_private_key_to_pem(pem_private_key_path, password=None):
+    pem_path = os.path.expanduser(pem_private_key_path)
+    with open(pem_path, "wb") as pem_file:
         private_key = rsa.generate_private_key(
             public_exponent=65537,
             key_size=2048,
@@ -27,7 +27,7 @@ def write_pem_private_key(pem_private_key_path, password=None):
             if password
             else serialization.NoEncryption()
         )
-        key_file.write(
+        pem_file.write(
             private_key.private_bytes(
                 encoding=serialization.Encoding.PEM,
                 format=serialization.PrivateFormat.TraditionalOpenSSL,
