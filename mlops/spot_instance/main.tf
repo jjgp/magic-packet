d617a925175f280f208dfcba97b3e7ff01fe7db0
Jason Grant Prasad <jasongprasad@gmail.com>
2021-12-10 10:14:24 +0000

    Add setup for developer ec2 instance (#1)
    * Add simple terraform setup
    
    * Add pre-commit and jupyter setup
    
    * Sort .gitignore sort -o .gitignore .gitignore
    
    * Remove tls from terraform lock
    
    * Add tag_name to main.tf
    
    * Add setup.py description
    
    * Refractor terraform/main.ipynb
    
    * Add writing of pem file
    
    * Rename main notebook to runbook
    
    * Reorg folder structure under mlops
    
    * Rename tag to reflect spot_instance
    
    * Fix read/write of pem
    
    * Add setup to be able to connect to EC2
    
    * Begin use of boto3
    
    * Add terraform pre-commit
    
    * Add additional pre-commit hooks
    
    * Add *.tfvars to .gitignore
    
    * Refactor directories for spot_instance
    
    * Refactor common functions to mlops module
    
    * Add spot pricing history
    
    * Simplify notebooks
    
    * Add on value changed
    
    * Add timedelta
    
    * Remove spot_instances.ipynb
    
    Revert this commit when revisiting
    
    * Change wording around pem
    
    * Start packer AMI
    
    * Work on volume attachment
    
    * Using t2.micro for dev
    
    * Work towards user_data and role assumption
    
    * Mounting ebs volume to instance
    
    * Move iam into it's own tf
    
    * Remove packer for now and update ebs script
    
    * Add comments to mlops/README.md

---
+++ mlops/spot_instance/main.tf
@@ -0,0 +1,128 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.68"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = var.tag_name
+  }
+}
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = var.tag_name
+  }
+}
+
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+
+  tags = {
+    Name = var.tag_name
+  }
+}
+
+resource "aws_subnet" "subnet" {
+  availability_zone = var.availability_zone
+  cidr_block        = "10.0.1.0/24"
+  vpc_id            = aws_vpc.vpc.id
+
+  tags = {
+    Name = var.tag_name
+  }
+}
+
+resource "aws_route_table_association" "route_table_association" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_security_group" "security_group" {
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 8888
+    to_port     = 8888
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.tag_name
+  }
+}
+
+resource "aws_key_pair" "key_pair" {
+  key_name   = var.key_name
+  public_key = var.public_key
+
+  tags = {
+    Name = var.tag_name
+  }
+}
+
+resource "aws_spot_instance_request" "spot_instance_request" {
+  spot_price                  = var.spot_price
+  ami                         = var.ami
+  wait_for_fulfillment        = true
+  associate_public_ip_address = true
+  availability_zone           = var.availability_zone
+  iam_instance_profile        = aws_iam_instance_profile.iam_instance_profile.name
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  monitoring                  = true
+  subnet_id                   = aws_subnet.subnet.id
+  user_data = templatefile("user_data.sh", {
+    availability_zone = var.availability_zone
+    region            = var.region
+    volume_id         = aws_ebs_volume.ebs_volume.id
+  })
+  vpc_security_group_ids = [aws_security_group.security_group.id]
+
+  tags = {
+    Name = var.tag_name
+  }
+}
+
+resource "aws_ebs_volume" "ebs_volume" {
+  availability_zone = var.availability_zone
+  size              = var.volume_size
+  type              = "gp2"
+
+  tags = {
+    Name = var.tag_name
+  }
+}
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+install_requires = []
+
+extras_require = {
+    "dev": ["black >= 21", "flake8 >= 4.0", "isort >= 5.9", "pre-commit >= 2.16"],
+    "mlops": [
+        "cryptography >= 36.0",
+        "jupyter >= 1.0",
+        "matplotlib >= 3.4",
+        # pandas is used by jupyter variables view in vscode interactive python window
+        "pandas >= 1.3",
+    ],
+}
+
+setup(
+    name="magic-packet",
+    version="0.0.1",
+    url="https://github.com/jjgp/magic-packet",
+    author="Jason Prasad",
+    author_email="jasongprasad@gmail.com",
+    description="Wake word detection",
+    install_requires=install_requires,
+    extras_require=extras_require,
+)
