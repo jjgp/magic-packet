6a70dae17e142f72c948256d8002e56b2d7fa7ee
Jason Prasad <jasongprasad@gmail.com>
2022-01-20 19:25:19 +0000

    Add demand instance mlops

---
+++ mlops/demand_instance/user_data.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Clone repository
+cd /home/ubuntu
+git clone "https://github.com/jjgp/magic-packet"
+chown -R ubuntu: magic-packet
---
+++ mlops/demand_instance/variables.tf
@@ -0,0 +1,40 @@
+variable "ami" {
+  default     = "ami-01a16356ed2a310d1"
+  description = "The AMI to use for the instance"
+  type        = string
+}
+
+variable "availability_zone" {
+  default     = "us-east-1a"
+  description = "The availability zone to run the instance"
+  type        = string
+}
+
+variable "instance_type" {
+  default     = "p2.xlarge"
+  description = "The EC2 instance type to use"
+  type        = string
+}
+
+variable "key_name" {
+  description = "The name to associate with the public key"
+  type        = string
+}
+
+variable "public_key" {
+  description = "The RSA public key to associate with the EC2 instance"
+  sensitive   = true
+  type        = string
+}
+
+variable "region" {
+  default     = "us-east-1"
+  description = "AWS region"
+  type        = string
+}
+
+variable "tag_name" {
+  default     = null
+  description = "Tag name to put on instances created by this terraform"
+  type        = string
+}
