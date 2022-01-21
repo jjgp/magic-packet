b67f640f0f364df60eadc139bdf8272b12cce70d
Jason Prasad <jasongprasad@gmail.com>
2022-01-21 10:14:30 +0000

    Add ubuntu environment

--- mlops/on_demand_instance/user_data.sh
+++ mlops/on_demand_instance/user_data.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-# Clone repository
 cd /home/ubuntu
+
+# Clone repository
 git clone "https://github.com/jjgp/magic-packet"
+chown -R ubuntu: magic-packet
+
+# Install magic-packet dependencies
+pushd magic-packet
+conda env create -y -f environments/ubuntu_x86_64/magic-packet.yml
+popd
+
+# Clone howl repository
+git clone "https://github.com/castorini/howl.git"
 chown -R ubuntu: magic-packet
