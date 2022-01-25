89efc8915476f13c7380d260991ea7641c56678d
Jason Prasad <jasongprasad@gmail.com>
2022-01-25 09:16:15 +0000

    Add createdb to cli

--- magic_packet/cli/common_voice/__init__.py
+++ magic_packet/cli/common_voice/__init__.py
@@ -1,11 +1,12 @@
 from magic_packet.cli.common_voice import download, extract
+from magic_packet.cli.common_voice.database import createdb
 
 
 def add_to_parser(parser):
     parser.description = "subcommands to assemble the common voice dataset"
     subparsers = parser.add_subparsers(required=True)
 
-    for module in (download, extract):
+    for module in (createdb, download, extract):
         name = module.__name__.split(".")[-1]
         parser = subparsers.add_parser(name)
         parser.set_defaults(func=module.main)
