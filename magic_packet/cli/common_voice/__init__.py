ad9398c24207e7f8125a51fabc14fbcf4208c5c0
Jason Prasad <jasongprasad@gmail.com>
2022-01-25 00:07:28 +0000

    Add description to cv

--- magic_packet/cli/common_voice/__init__.py
+++ magic_packet/cli/common_voice/__init__.py
@@ -2,6 +2,7 @@ from magic_packet.cli.common_voice import download
 
 
 def add_to_parser(parser):
+    parser.description = "subcommands to assemble the common voice dataset"
     subparsers = parser.add_subparsers(required=True)
     parser_download = subparsers.add_parser("download")
     parser_download.set_defaults(func=download.main)
--- magic_packet/cli/mgpkt.py
+++ magic_packet/cli/mgpkt.py
@@ -6,8 +6,10 @@ from magic_packet.cli import common_voice
 def main():
     parser = argparse.ArgumentParser(description="the magic packet cli")
     subparsers = parser.add_subparsers(required=True)
+
     parser_cv = subparsers.add_parser("common_voice", aliases=["cv"])
     common_voice.add_to_parser(parser_cv)
+
     args = parser.parse_args()
     args.func(args)
 
