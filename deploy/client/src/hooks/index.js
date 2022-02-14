09049baea120eac14da5bcfcd65d69cc2cefbac3
Jason Prasad <jasongprasad@gmail.com>
2022-02-14 09:52:37 +0000

    Remove media recorder

--- deploy/client/src/hooks/index.js
+++ deploy/client/src/hooks/index.js
@@ -1,4 +1,3 @@
 export { useAudioContext } from "./useAudioContext";
 export { useAnalyserRecorder } from "./useAnalyserRecorder";
-export { useMediaRecorder } from "./useMediaRecorder";
 export { useSourceAnalyser } from "./useSourceAnalyser";
